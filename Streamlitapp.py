b2d050fab3bae07e30baee5c54c8de0d6f4721df
MadhavChandrasekher <64166232+MadhavChandrasekher@users.noreply.github.com>
2020-06-17 12:41:08 -0700

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -8,15 +8,15 @@ st.title("EtsyEdge")
 #@st.cache
 #def load_toys(path):
 #    return pd.read_csv(path)
-df_candles = st.cache(pd.read_csv)('https://github.com/MadhavChandrasekher/Projects/blob/master/streamlit_candles.csv') 
+df_candles = st.cache(pd.read_csv)('streamlit_candles.csv') 
 df_candles= df_candles.sort_values(by=['company','product'])
 d=list(df_candles['betabestseller2'])
 y_c=d[0]
-df_earrings = st.cache(pd.read_csv)('https://github.com/MadhavChandrasekher/Projects/blob/master/cleaned_data_earrings_subsample.csv')
+df_earrings = st.cache(pd.read_csv)('cleaned_data_earrings_subsample.csv')
 df_earrings = df_earrings.sort_values(by=['company','product'])
 d=list(df_earrings['betabestseller2'])
 y_e=d[0]
-df_toys = st.cache(pd.read_csv)('https://github.com/MadhavChandrasekher/Projects/blob/master/streamlit_woodentoys.csv')
+df_toys = st.cache(pd.read_csv)('streamlit_woodentoys.csv')
 df_toys = df_toys.sort_values(by=['company','product'])
 d=list(df_toys['betabestseller2'])
 y_t=d[0]
