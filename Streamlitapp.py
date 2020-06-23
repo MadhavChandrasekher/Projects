3c723c2bdb065755815529f1924d0dc27806bc11
Madhav Chandrasekher <64166232+MadhavChandrasekher@users.noreply.github.com>
2020-06-23 15:52:26 -0700

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -6,6 +6,7 @@ import csv
 from numpy import loadtxt
 import streamlit as st
 import seaborn as sns
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier 
